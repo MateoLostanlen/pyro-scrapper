834a05c82e38793f58acebf9b7183c3954ad8528
Mateo <mateo.lostanlen@gmail.com>
2023-07-09 17:52:47 +0200

    fix scrapper

--- scripts/awf_scrap.py
+++ scripts/awf_scrap.py
@@ -18,7 +18,7 @@ import subprocess
 now = datetime.now()
 
 DURATION = "6h"  # options: 15m, 1h, 3h, 6h, 12h
-OUTPUT_PATH = "dl_frames/" + now.isoformat().split(".")[0].replace("-", "_").replace(
+OUTPUT_PATH = "/hdd/dl_frames/" + now.isoformat().split(".")[0].replace("-", "_").replace(
     ":", "_"
 )
 CAMERAS_URL = (
@@ -107,48 +107,6 @@ with multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1) as pool:
     results = tqdm(pool.imap(remove_if_gray, imgs), total=len(imgs))
     tuple(results)
 
-
-# Run prediction
-scrap_folders = glob.glob(OUTPUT_PATH + "/*")
-scrap_folders.sort()
-
-weight = "celestial-armadillo-11.pt"
-
-for scrap_folder in tqdm(scrap_folders):
-    name = scrap_folder.split("/")[-2] + "_" + scrap_folder.split("/")[-1]
-
-    cmd = f"yolo predict model={weight} conf=0.25 source={scrap_folder} save=False save_txt save_conf name={name}"
-    print(f"* Command:\n{cmd}")
-    subprocess.call(cmd, shell=True)
-
-
-# Keep only positive detections
-for scrap_folder in tqdm(scrap_folders):
-    name = scrap_folder.split("/")[-2] + "_" + scrap_folder.split("/")[-1]
-    labels = glob.glob(f"runs/detect/{name}/labels/*")
-    labels.sort()
-    time_zone = []
-    keep = set()
-    imgs = glob.glob(scrap_folder + "/*")
-    if len(labels) > 0:
-
-        for label in labels:
-
-            t = os.path.basename(label).split(".txt")[0]
-            t = datetime.strptime(t, "%Y_%m_%dT%H_%M_%S")
-            t_min = t - timedelta(minutes=15)
-            t_max = t + timedelta(minutes=15)
-            for file in imgs:
-                t = os.path.basename(file).split(".jpg")[0]
-                t = datetime.strptime(t, "%Y_%m_%dT%H_%M_%S")
-                if t > t_min and t < t_max:
-                    keep.add(file)
-
-    for file in imgs:
-        if not file in keep:
-            os.remove(file)
-
-    print(name, len(keep), len(imgs))
 
 
 ## clean empty folders
